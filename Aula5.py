22cfc9934b716e28c4317b056cba06e1e63c7e89
Victor Hugo Rodrigues da Silva <victorq32@gmail.com>
2016-12-27 17:38:47 -0200

    exercicios

---
+++ Aula5.py
@@ -0,0 +1,10 @@
+
+#imprime o nome na tela
+print("Victor Hugo")
+
+print("Minha idade é",24,"anos")
+'''
+sakdçal
+daslkçdjlçsa
+kjlçasd
+'''
---
+++ variaveis.py
@@ -0,0 +1,22 @@
+x,y = 25,20;
+#x =y = 25;
+print ("Victor Hugo e tenho", x,"anos");
+print ("Victor Hugo e tenho", y,"anos");
+
+#idade_victor = 24;
+
+soma = x + y;
+sub = x - y;
+mult = sub * x;
+div = y / sub;
+potencia = 2**2;
+
+# div = y / x + 10 *2;
+
+print(soma);
+print(sub);
+print(mult);
+print(div);
+print(potencia);
+
+print("A soma de", x, "+", y, "é", x +y)
